483255c6d8335f3885da576c1b7c100dadf99264
yescine <bouchoucha.yassine@gmail.com>
2022-04-10 17:27:44 +0100

    basic board health-check signals

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+void blinkLed (byte gpio);
+void startingLoop ();
+
+// !touch only led
+const byte yellowLed = 32;
+byte loopFlag = 0;
+
+void setup() {
+  pinMode(yellowLed, OUTPUT);
+  Serial.begin(921600);
+  Serial.println("\nStarting the setup");
+}
+
+void loop() {
+  startingLoop();
+  digitalWrite(yellowLed, HIGH);
+  delay(1000);
+  digitalWrite(yellowLed, LOW);
+  delay(1000);
+}
+
+void blinkLed (byte gpio) {
+  delay(1000);
+  digitalWrite(gpio, 0x1);
+  delay(1000);
+  digitalWrite(gpio, 0x0);
+}
+
+void startingLoop(){
+  if (loopFlag>0) return;
+  Serial.println("Starting the loop...");
+  loopFlag++;
+
+}
