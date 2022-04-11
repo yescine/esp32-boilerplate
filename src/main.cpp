02192abc4b6a94713b7ffee28df99890aa26ba25
yescine <bouchoucha.yassine@gmail.com>
2022-04-11 06:59:28 +0100

    add rgb led controll

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
-
+#include <driver/gpio.h>
+#include <RGBLed.h>
 void blinkLed (byte gpio);
 void startingLoop ();
 
 // !touch only led
-const byte yellowLed = 32;
+const byte yellowLed = GPIO_NUM_32;
 byte loopFlag = 0;
+
+const byte rgbLed[] = {GPIO_NUM_26,GPIO_NUM_25,GPIO_NUM_23};
+RGBLed rainbow(rgbLed[0],rgbLed[1],rgbLed[2],false);
 
 void setup() {
   pinMode(yellowLed, OUTPUT);
@@ -16,6 +20,7 @@ void setup() {
 void loop() {
   startingLoop();
   blinkLed(yellowLed);
+  rainbow.crossFade(255, 0, 0, 0, 255, 255, 8, 400);
 }
 
 void blinkLed (byte gpio) {
